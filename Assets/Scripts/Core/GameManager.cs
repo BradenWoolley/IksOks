6dc7b5ddb73739d1730a98bc01feb9c88820d144
Braden Woolley <braden.mcw@gmail.com>
2026-03-12 01:23:49 +0100

    IO-0004 Fixed player move bug.

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -108,7 +108,7 @@ public class GameManager : MonoBehaviour
     {
         if (Instance != null && Instance != this)
         {
-            Destroy(this);
+            Destroy(gameObject);
         }
 
         else
